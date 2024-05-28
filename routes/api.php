6f0c465618105ad8b199a9b4bf7ddd2ba09c1680
MariaLuizaAdame33 <maluzinha.adame@gmail.com>
2024-05-28 15:36:55 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('cadastroRoupas',[RoupasController::class,'cadastroRoupas']);
 Route::get('pesquisaCategoria',[RoupasController::class,'pesquisarPorCategoria']);
-Route::delete('excluir/{id}',[RoupasController::class,'excluir']);
+Route::get('excluir/{id}',[RoupasController::class,'excluir']);
 Route::get('vizualizar',[RoupasController::class,'retornarTodos']);
 Route::put('editar/{id}',[RoupasController::class,'editar']);
 
